8a1d890ca6069ee353297de892bf3ca4d32842fb
Ashok Kumar <stranger9811@hotmail.com>
2014-04-08 11:28:29 +0530

    temp

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -381,11 +381,11 @@ end
 	  	if params[:password]!=params[:repassword]
 	  		@error = "password do not match"
 	  	end
-	  	user = User.find_by_sql("select * from non_verified_users where email=\""+params[:email]+"\"")
+	  	user = User.where(email=\""+params[:email]+"\"")
 	  	if user[0]!=nil
 	  		@error = "user already exists"
 	  	end
-	  	user = User.find_by_sql("select * from users where email=\""+params[:email]+"\"")
+	  	user = User.where(:email => params[:email])
 	  	if user[0]!=nil
 	  		@error = "user already exists"
 	  	end
